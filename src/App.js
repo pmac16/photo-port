060e4b8acac2815fa66a7f5205d2e0bc7e250000
Priya Macpherson <priyamacpherson@Priyas-MacBook-Pro.local>
2021-04-18 10:33:20 -0700

    created Nav compomentnt

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
 import React from 'react';
+import Nav from './components/Nav';
 import About from './components/About';
 
 function App() {
   return (
     <div>
+      <Nav></Nav>
       <main>
         <About></About>
       </main>
-
     </div>
-  )
+  );
 }
 
 export default App;
