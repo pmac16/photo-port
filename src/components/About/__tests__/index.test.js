1df8e83855327b744fbec89aef58e85b5c913e61
Priya Macpherson <priyamacpherson@Priyas-MacBook-Pro.local>
2021-04-19 21:27:04 -0700

    set up About testing file

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import {render, cleanup} from '@testing-library/react';
+
+//render: this will render the component
+//cleanup: remove components from the DOM to prevent memory leaking
+
+import '@testing-library/jest-dom/extend-expect';
+
+import About from '..';
+
+afterEach(cleanup); //afterEach is a global function from Jest
+//this ensures that after each test, we will not have any leftover memory data that could give us false results
+
+describe('About component', () => { //describe is used to declare the compoenent we are testing 
+    //First Test
+
+
+    //Second Test
+})
